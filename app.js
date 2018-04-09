41f893ae747b6c2a9f9ce307050c2215632ff7a8
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-04-09 08:41:55 -0400

    add views directory and rsvp view

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get('/', function(req, res){
-  res.send("landing");
+  res.render("home");
+});
+
+app.get('/rsvp', function(req, res){
+  res.render("rsvp");
 });
 
 
