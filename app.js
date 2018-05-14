b4e3bec772798e2511b4d636e8af41e6d650edc0
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-05-14 00:07:24 -0400

    database configuration

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
+var mongoose = require("mongoose");
+
 
 app.set("view engine", "ejs");
 
+//MODELS
+var Rsvp = require('./models/rsvp');
+
 // ROUTES
 var routes = require("./routes/index");
-//Route configuration
+
+// DATABASE CONFIG
+mongoose.connect("mongodb://amanda:" + process.env.MLABRSVPPW + "@ds221990.mlab.com:21990/wedding-guestlist",  {useMongoClient: true});
+mongoose.Promise = global.Promise;
+
+//ROUTE CONFIGURATION
 app.use(routes);
 
 
