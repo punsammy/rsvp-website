802e324e7702e29c1c0a27bb49cc27e6144676fa
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-05-14 00:52:06 -0400

    rsvp modal config

---
+++ models/rsvp.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var RsvpSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  guests: {
+    type: Number,
+    required: true
+  },
+  phoneNumber: {
+    type: String
+  },
+  comment: {
+    type: String
+  }
+});
