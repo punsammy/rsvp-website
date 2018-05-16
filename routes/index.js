747bda1197fe1f0fa132713635e3d79f76e672f5
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-05-16 19:00:03 -0400

    configure post route for rsvp

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express  = require("express"),
-    router   = express.Router();
+    bodyParser = require("body-parser"),
+    router   = express.Router(),
+    Rsvp     = require('../models/rsvp');
 
 router.get('/', function(req, res){
   res.render("home");
@@ -7,6 +9,24 @@ router.get('/', function(req, res){
 
 router.get('/rsvp', function(req, res){
   res.render("rsvp");
+});
+
+router.post('/rsvp', function(req, res){
+  var newRsvp = new Rsvp({
+    name: req.body.name.toLowerCase(),
+    email: req.body.email.toLowerCase(),
+    guests: req.body.guests,
+    phoneNumber: req.body.phoneNumber,
+    comment: req.body.comment
+  });
+  Rsvp.create(newRsvp, function(err, newlyCreated){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('success');
+      res.redirect('/');
+    }
+  });
 });
 
 
