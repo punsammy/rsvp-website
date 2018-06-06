005f276c8c80b97e9b5aea4b63eb41c8dae84c98
Amanda Punsammy <amanda.punsammy@gmail.com>
2018-06-05 20:13:45 -0400

    change delay time for hero-title

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,5 @@ $(document).ready(function(){
   $('html').click(function(){
     $('.modal').fadeOut('fast');
   });
+
 });
--- public/js/scrollreveal.js
+++ public/js/scrollreveal.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.hero-text', {delay: 300, duration: 600});
+sr.reveal('.hero-text', {delay: 500, duration: 600});
 sr.reveal('.wedding-info', {duration: 600});
 sr.reveal('.details-rsvp-button', {duration: 600});
